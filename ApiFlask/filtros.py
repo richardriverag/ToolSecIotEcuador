43727d37a7f660fe350f890b6d3c08b9782c81da
EdisonStalin[C <edison.jumbo@epn.edu.ec>
2021-12-14 02:37:45 -0500

    delete2

--- ApiFlask/filtros.py
+++ ApiFlask/filtros.py
@@ -40,10 +40,30 @@ def datacity(capitalize):
         cantidad = port_filter.count()
         if cantidad != 0:
             #add port_info
-            puerto = {"Puerto": str(x), "Cantidad": cantidad}
+            puerto = (str(x), cantidad)
             port_Info.append(puerto)
 
     return port_Info
+
+
+def dataPort ():
+
+    db = getDevice_db()
+
+    PortList = [22, 23, 25, 53, 80, 81, 110, 180, 443, 873, 2323, 5000, 5001, 5094, 5150, 5160, 7547, 8080, 8100, 8443, 8883, 49152, 52869, 56000,
+                        1728, 3001, 8008, 8009, 10001, 223, 1080, 1935, 2332, 8888, 9100, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 21, 554, 888, 1159, 1160, 1161,
+                        1435, 1518, 3389, 4550, 5005, 5400, 5550, 6550, 7000, 8000, 8081, 8090, 8150, 8866, 9000, 9650, 9999, 10000, 18004, 25001, 30001, 34567, 37777,
+                        69, 135, 161, 162, 4786, 5431, 8291, 37215, 53413]
+
+    data = []
+    
+    for port in PortList:
+        countPort = db.find({'puerto.Puerto': str(port)})
+        quantity = countPort.count()
+        port_information = (port, quantity)
+        data.append(port_information)
+        
+    return data
 
 
 ##Contraseña
